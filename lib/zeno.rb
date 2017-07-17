baa0c1f21f0c7af94c090b282009bb7d51bf305c
Michel Megens <dev@bietje.net>
2017-07-17 16:12:00 +0200

    version: add license header
    Add a license header to the version file.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- lib/zeno.rb
+++ lib/zeno.rb
@@ -1,6 +1,6 @@
 #
 #   Zeno module
-#   Copyright (C) 2016  Michel Megens <dev@bietje.net>
+#   Copyright (C) 2016, 2017  Michel Megens <dev@bietje.net>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
