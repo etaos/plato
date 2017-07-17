88cb9e9c06c1f4371f329b76ebf612634b6b8916
Michel Megens <dev@bietje.net>
2017-07-17 16:13:47 +0200

    spec: update Makefile generator spec
    Update the Makefile generation spec.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- spec/makefile_spec.rb
+++ spec/makefile_spec.rb
@@ -9,11 +9,11 @@ describe Zeno::Makefile do
 
   it 'generates output as expected' do
     mkfile = Zeno::Makefile.new 'tmp'
-    expected_output = "VAR1=/some/path\nVAR2=/another/path\n\n"
+    expected_output = "VAR1 := /some/path\nVAR2 = /another/path\n\n"
     expected_output += "all:\n\tgcc -o test.o -c test.c\n\n"
 
-    mkfile.add_var('VAR1', '/some/path')
-    mkfile.add_var('VAR2', '/another/path')
+    mkfile.add_var('VAR1', '/some/path', ':=')
+    mkfile.add_var('VAR2', '/another/path', '=')
     mkfile.add_target('all', 'gcc -o test.o -c test.c')
 
     expect(expected_output).to eq(mkfile.to_s)
